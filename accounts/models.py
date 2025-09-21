ef0a8e0392cbd81c7ca6ff3ffa9159014076dfd7
Kevin Kim <kimanikevin254@gmail.com>
2025-09-21 20:17:04 +0300

    add custom user manager

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class UserManager(BaseUserManager):
+    """Custom user manager for email-based authentication"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and return a regular user with email"""
+        if not email:
+            raise ValueError('Email field cannot be empty')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        # Only set passwords if provided(for superusers)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_unusable_password() # Mark as no password for social auth users
+
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and return a superuser"""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("user_type", 'admin') # Make users admin type
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
+    # Remove username field
+    username = None
+
     # Define user types, as we'll have customers and admins
     USER_TYPES = (
         ('customer', 'Customer'),
@@ -14,8 +50,9 @@ class User(AbstractUser):
     address = models.TextField(blank=True)
 
     USERNAME_FIELD = 'email' # Use email as the login field
-    
     REQUIRED_FIELDS = ['first_name', 'last_name'] # Required fields when creating a superuser
+
+    objects = UserManager() # use the custom manager
 
     def __str__(self):
         return self.email
